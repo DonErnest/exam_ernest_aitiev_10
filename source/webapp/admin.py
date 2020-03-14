c460e89ad550cbfac47af9e15362446a6cec5c34
ernest_aitiev <valeetmihifaveas@gmail.com>
2020-03-14 10:06:17 +0600

    Создана модель Файлов. Поля: файл, название, автор, счетчик скачиваний, дата загрузки, тип доступа, список пользователей с приватным способом. Проведены миграции, настроена панель джанго-админа

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import SharedFile
+
+
+class FileModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'file', 'sharing_type', 'user_id', 'uploaded', 'downloaded_count', 'privately_accessed']
+    list_display = ['id', 'name', 'file', 'sharing_type', 'user_id', 'uploaded', 'downloaded_count']
+    list_filter = ['user_id', 'sharing_type']
+    readonly_fields = ['uploaded']
+    search_fields = ['name']
+
+
+admin.site.register(SharedFile, FileModelAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+ACCESS_PUBLIC = 'public'
+ACCESS_CLOSED = 'closed'
+ACCESS_PRIVATE = 'private'
+ACCESS_TYPE = [(ACCESS_PUBLIC, "публичный"), (ACCESS_CLOSED, "скрытый"), (ACCESS_PRIVATE, "приватный")]
+
+
+class SharedFile(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Подпись')
+    file = models.FileField(upload_to='files/', verbose_name='Файл')
+    sharing_type = models.CharField(max_length=10, choices=ACCESS_TYPE, default=ACCESS_PUBLIC, verbose_name='Тип доступа')
+    user_id = models.ForeignKey(User, null=True, blank=True, verbose_name='Загрузивший пользователь', on_delete=models.CASCADE)
+    uploaded = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    downloaded_count = models.IntegerField(default=0, verbose_name='Скачан')
+    privately_accessed = models.ManyToManyField(to=User, related_name='private_files', verbose_name='Приватный доступ')
